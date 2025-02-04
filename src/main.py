b66edbd5d1eb127cde3881b69efe327199c49e79
Anika Jha <113995788+Anika-Jha@users.noreply.github.com>
2025-02-04 18:05:08 +0530

    Add files via upload

---
+++ src/main.py
@@ -0,0 +1,7 @@
+def main():
+    print("Welcome to Comp_Srch!")
+    compound_name = input("Enter compound name: ")
+    print(f"Searching databases for: {compound_name}")
+
+if __name__ == "__main__":
+    main()
