79c99af2036440c775c718e1bb9f18d14b53257c
Anika Jha <113995788+Anika-Jha@users.noreply.github.com>
2025-03-17 15:57:08 +0530

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,7 @@
 import re
-#Cleans and formats compound names for consistency.
+
 def clean_compound_name(name):
-    
-    name = name.strip()  
-    name = re.sub(r"\s+", " ", name)  
+    # Cleans and formats compound names for consistency.
+    name = name.strip()  # Remove leading/trailing spaces
+    name = re.sub(r"\s+", " ", name)  # Replace multiple spaces with a single space
     return name
