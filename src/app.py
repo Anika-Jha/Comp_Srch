bab598948b814de7438cf75a547fc72bc4d4c087
Anika Jha <113995788+Anika-Jha@users.noreply.github.com>
2025-07-06 21:43:18 +0530

    Update app.py

--- src/app.py
+++ src/app.py
@@ -28,7 +28,7 @@ st.title("🧪 COMP_SRCH")
 st.caption("Compound search app")
 st.markdown("Search compounds and retrieve **PubChem, KEGG, HMDB, CAS identifiers**. HMDB search uses fuzzy match by default.")
 
-# ------------------------- FUNCTION TO CHECK DB STATUS -------------------------
+# ------------------------- FUNCTION TO CHECK DB STATUS (pubchem, KEGG and HMDB)-------------------------
 
 def check_db_status():
     services = {
