3d6a4d49c136785305ebd0883f36479b4b01e308
Anika Jha <113995788+Anika-Jha@users.noreply.github.com>
2026-01-06 20:23:33 +0530

    Update dossier.py

--- src/dossier.py
+++ src/dossier.py
@@ -2,15 +2,16 @@
 
 def generate_dossier(name, ids_dict, synonyms):
     lines = [
-        f"🧪 Compound Dossier",
+        f" Compound Dossier",
         f"{'=' * 30}",
         f"Name: {name}",
         f"Synonyms: {synonyms or 'Unavailable'}\n",
-        f"📛 Identifiers:"
+        f" Identifiers:"
     ]
     for db, val in ids_dict.items():
         lines.append(f" - {db}: {val or 'Unavailable'}")
 
-    lines.append("\n🔎 Source: Comp_Srch CLI")
+    lines.append("\n Source: Comp_Srch CLI")
     lines.append("=" * 30)
     return "\n".join(lines)
+#change to CIE
